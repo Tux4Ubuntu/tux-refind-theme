ebc5e0ba4c098f640c6d457600c84ec6ca2331da
tuxedojoe <nottoday@gmail.com>
2018-08-01 02:07:03 +0200

    Graphical updates

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 # Set global values
 STEPCOUNTER=false # Changes to true if user choose to install Tux Everywhere
-RED='\033[0;31m'
+YELLOW='\033[1;33m'
+LIGHT_GREEN='\033[1;32m'
+LIGHT_RED='\033[1;31m'
 NC='\033[0m' # No Color
 
 function install {
     printf "\033c"
-    header "Adding TUX as REFIND THEME" "$1"
-    printf "${RED}MAKE SURE rEFInd IS INSTALLED!${NC}\n"                       
-    echo ""
+    header "TUX REFIND THEME" "$1"
     echo "rEFInd does many things and one thing is that its presenting the boot options"
     echo "in a more graphical way. Using rEFInd makes it possible to have Tux on the"
     echo "Ubuntu Boot Drive for example. Install instructions can be found here:"
@@ -15,12 +15,12 @@ function install {
     echo ""
     echo "PS. If you're not booting different OSes on your PC, don't bother with rEFInd."
     echo ""
-    echo "Already up and running with rEFInd?"
+    printf "${LIGHT_GREEN}Already up and running with rEFInd?${NC}\n"
     echo "(Type 1 or 2, then press ENTER)"
     select yn in "Yes" "No"; do
         case $yn in
             Yes )
-                echo "Initiating to copy folder tux-refind-theme."
+                printf " ${YELLOW}Initiating to copy folder tux-refind-theme.${NC}\n"
                 check_sudo
                 if [ -d "/boot/efi/EFI/refind/themes/" ]; then
                 # Control will enter here if $DIRECTORY exists.
@@ -29,11 +29,11 @@ function install {
                     sudo cp -rf tux-refind-theme /boot/efi/EFI/refind/themes/tux-refind-theme
                     # Here we add a last line if it not already exists (If other themes exists doesn't matter since our line ends up last and will therefore be used)
                     sudo grep -q -F 'include themes/tux-refind-theme/theme.conf' /boot/efi/EFI/refind/refind.conf || echo 'include themes/tux-refind-theme/theme.conf' | sudo tee -a /boot/efi/EFI/refind/refind.conf
-                    echo "Successfully copied 'tux-refind-theme' to your rEFInd themes folder."
+                    printf "${YELLOW}Successfully copied 'tux-refind-theme' to your rEFInd themes folder.${NC}\n"
                 else
                     printf "\033c"
-                    header "Adding TUX as REFIND THEME" "$1"
-                    printf "${RED}COULDN'T FIND REFIND THEMES FOLDER!${NC}\n"   
+                    header "TUX REFIND THEME" "$1"
+                    printf "${LIGHT_RED}Couldn't find rEFInd's theme folder${NC}\n"   
                     echo "If rEFInd is installed, check out our manual instructions at:"
                     echo "https://tux4ubuntu.org"
                     echo ""
@@ -41,8 +41,8 @@ function install {
                 fi
                 break;;
             No ) printf "\033c"
-                header "Adding TUX as REFIND THEME" "$1"
-                echo "Exiting rEFInd installer."
+                header "TUX REFIND THEME" "$1"
+                printf "${YELLOW}Exiting rEFInd installer.${NC}\n"
 
                 break;;
         esac
@@ -54,13 +54,13 @@ function install {
 
 function uninstall { 
     printf "\033c"
-    header "Removing Tux in BOOT LOADER" "$1"
-    echo "Are you sure you want to remove TUX REFIND THEME from your rEFInd?"
+    header "TUX REFIND THEME" "$1"
+    printf "${LIGHT_RED}Are you sure you want to remove TUX REFIND THEME from your rEFInd?${NC}\n"
     echo ""
     echo "(Type 1 or 2, then press ENTER)"
     select yn in "Yes" "No"; do
         case $yn in
-            Yes ) header "Removing Tux to BOOT LOADER" "$1"
+            Yes ) header "TUX REFIND THEME" "$1"
 
                 if [ -d "/boot/efi/EFI/refind/themes/tux-refind-theme" ]; then
                 # Control will enter here if $DIRECTORY exists.
@@ -70,21 +70,21 @@ function uninstall {
                     theme_line="include themes/tux-refind-theme/theme.conf"
                     if [ "$last_line" == "$theme_line" ] 
                     then
-                        echo "Removing theme from rEFInd."
+                        printf " ${YELLOW}Removing theme from rEFInd.${NC}\n"
                         sudo sed -i '$ d' /boot/efi/EFI/refind/refind.conf
-                        sudo rm -f -r /efi/EFI/boot/refind/themes/tux-refind-theme   
-                        echo "Successfully removed the theme from rEFInd."                                 
+                        sudo rm -f -r /efi/EFI/boot/refind/themes/tux-refind-theme
+                        printf " Successfully removed the theme from rEFInd.\n"                      
                     else
                         printf "\033c"
-                        header "Removing Tux to BOOT LOADER" "$1"
+                        header "TUX REFIND THEME" "$1"
                         echo "Seems like someone edited the refind.conf file. For more information and help:"
                         echo "https://tux4ubuntu.org"
                     fi
 
                 else
                     printf "\033c"
-                    header "Adding TUX REFIND THEME" "$1"
-                    printf "${RED}COULDN'T FIND REFIND TUX-REFIND-THEME FOLDER!${NC}\n"   
+                    header "TUX REFIND THEME" "$1"
+                    printf "${LIGHT_RED}Couldn't find 'tux-refind-theme'-folder${NC}\n"
                     echo "If the theme is still installed, check out our manual instructions at:"
                     echo "https://tux4ubuntu.org"
                 fi
@@ -93,7 +93,7 @@ function uninstall {
                 break;;
             No ) printf "\033c"
                 header "Removing Tux to BOOT LOADER" "$1"
-                echo "Awesome! Tux smiles and gives you a pat on the shoulder."
+                echo " Awesome! Tux smiles and gives you a pat on the shoulder."
             break;;
         esac
     done
@@ -117,10 +117,10 @@ function header {
     ch=' '
     echo "╔══════════════════════════════════════════════════════════════════════════════╗"
     printf "║"
-    printf " $1"
+    printf " ${YELLOW}$1${NC}"
     printf '%*s' "$len" | tr ' ' "$ch"
     if [ $STEPCOUNTER = true ]; then
-        printf "Step "$2
+        printf "Step "${LIGHT_GREEN}$2${NC}
         printf "/7 "
     fi
     printf "║\n"
@@ -132,45 +132,47 @@ function check_sudo {
     if sudo -n true 2>/dev/null; then 
         :
     else
-        echo "Oh, and Tux will need sudo rights to copy and install everything, so he'll ask" 
-        echo "about that soon."
-        echo ""
+        printf "${YELLOW}Oh, TUX will ask below about sudo rights to copy and install everything...${NC}\n\n"
     fi
 }
 
 function goto_tux4ubuntu_org {
     echo ""
-    echo "Launching website in your favourite browser."
+    printf "${YELLOW}Launching website in your favourite browser...${NC}\n"
     x-www-browser https://tux4ubuntu.org/ &
-    read -n1 -r -p "Press any key to continue..." key
     echo ""
+    sleep 2
+    read -n1 -r -p "Press any key to continue..." key
 }
 
 while :
 do
     clear
+    if [ -z "$1" ]; then
+        :
+    else
+        STEPCOUNTER=true
+    fi
+    header "TUX REFIND THEME" "$1"
     # Menu system as found here: http://stackoverflow.com/questions/20224862/bash-script-always-show-menu-after-loop-execution
-    cat<<EOF    
-╔══════════════════════════════════════════════════════════════════════════════╗
-║ TUX REFIND THEME ver 1.0                                   © 2018 Tux4Ubuntu ║
-║ Let's Bring Tux to Ubuntu                             https://tux4ubuntu.org ║
-╠══════════════════════════════════════════════════════════════════════════════╣
-║                                                                              ║
-║   What do you wanna do today? (Type in one of the following numbers)         ║
-║                                                                              ║
-║   1) Read manual instructions                  - Open up tux4ubuntu.org      ║
-║   2) Install                                   - Install the theme           ║
-║   3) Uninstall                                 - Uninstall the theme         ║
-║   ------------------------------------------------------------------------   ║
-║   Q) Quit                                      - Quit the installer (Ctrl+C) ║
-║                                                                              ║
-╚══════════════════════════════════════════════════════════════════════════════╝
+    cat<<EOF                                                                      
+Type one of the following numbers/letters:
+                                                         
+1) Read Instructions                      - Open up tux4ubuntu.org      
+2) Install                                - Install Boot Loader theme          
+3) Uninstall                              - Uninstall Boot Loader theme       
+--------------------------------------------------------------------------------   
+Q) Skip                                   - Quit Boot Loader theme installation
+
+(Press Control + C to quit the installer all together)
 EOF
     read -n1 -s
     case "$REPLY" in
     "1")    goto_tux4ubuntu_org;;
     "2")    install $1;;
     "3")    uninstall $1;;
+    "S")    exit                      ;;
+    "s")    exit                      ;;
     "Q")    exit                      ;;
     "q")    exit                      ;;
      * )    echo "invalid option"     ;;
